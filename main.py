9111f7cd552b596d934fdd824f7377c4bdafd273
Aayush3014 <ayushak3008@gmail.com>
2023-03-30 16:16:17 +0530

    Initialising window size for the game

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import random
 import sys
 import pygame
 from pygame.locals import *
+
+#Global variables for game
+
+FPS = 32   # Frames per second
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+
+SCREEN = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+
