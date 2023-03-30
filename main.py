a7539dc99a84deab8bf0bf3f774cf80a0773aa20
Aayush3014 <ayushak3008@gmail.com>
2023-03-30 16:32:14 +0530

    Initialising Global Variables

--- main.py
+++ main.py
@@ -9,5 +9,24 @@ FPS = 32   # Frames per second
 SCREENWIDTH = 289
 SCREENHEIGHT = 511
 
-SCREEN = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+# Initializing Window Size for the Game
+SCREEN = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT)) 
 
+# Assigning 80% area of the screen to the image of the ground.
+GROUNDY = 0.8
+
+# For displaying the images
+GROUND_SPRITES = {}
+
+#For playing the sounds.
+GROUND_SOUNDS = {}
+
+PLAYER = '/gallery/sprites/bird.png'
+BACKGROUND = '/gallery/sprites/background.png'
+PIPE = '/gallery/sprites/pipe.png'
+
+if __name__ == "__main__":
+    
+    # This is the main point from where our game will start.
+    pygame.init()
+    
